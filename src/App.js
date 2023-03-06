833a5c3137c18786e9e0f0f6d5c0ff7aa128dc4f
Aqtar Parveez <aqtarparveez213@gmail.com>
2023-03-06 23:37:59 +0530

    Starting site

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This is now Aqtars portfolio Website
+      </header>
+      <p>And its gonna be a awsome site to lookm at B****</p>
+    </div>
+  );
+}
+
+export default App;
